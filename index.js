0ce57f4004a15c7b273b28568f316918abdaeebb
emonahamed <emonahamed711@gmail.com>
2022-11-09 23:04:15 +0600

    review deleted

--- index.js
+++ index.js
@@ -45,6 +45,28 @@ async function run() {
             res.send(service);
         });
 
+        // reviews api
+
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
         app.post('/reviews', async (req, res) => {
             const review = req.body;
