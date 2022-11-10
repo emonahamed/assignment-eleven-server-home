870a09893676216427cc8d478624f5ceb65d19ac
emonahamed <emonahamed711@gmail.com>
2022-11-10 11:54:34 +0600

    service added

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ async function run() {
             res.send(services);
         });
 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
+
+
+
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
